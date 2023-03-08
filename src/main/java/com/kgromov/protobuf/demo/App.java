f7bbd9650bd8463fc7218568ea9e623159dac1d7
Konstantin <konstantin@revimedia.com>
2023-03-09 00:32:06 +0200

    Init commit

---
+++ src/main/java/com/kgromov/protobuf/demo/App.java
@@ -0,0 +1,8 @@
+package com.kgromov.protobuf.demo;
+
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/com/kgromov/protobuf/demo/HelloWorldTest.java
@@ -0,0 +1,58 @@
+package com.kgromov.protobuf.demo;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+import org.junit.Test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class HelloWorldTest {
+
+    @Test
+    public void buildDefaultRequest_valuesAreEmpty() {
+        HelloWorldRequest defaultInstance = HelloWorldRequest.getDefaultInstance();
+        String requestId = defaultInstance.getRequestId();
+        String message = defaultInstance.getMessage();
+
+        assertThat(defaultInstance.getRequestId()).isNullOrEmpty();
+        assertThat(defaultInstance.getMessage()).isNullOrEmpty();
+    }
+
+    @Test
+    public void buildRequestFromBytes_DeserializedValuesAreTheSame() throws InvalidProtocolBufferException {
+        HelloWorldRequest request = HelloWorldRequest.newBuilder()
+                .setRequestId("My request id")
+                .setMessage("Some message")
+                .build();
+        byte[] requestBytes = request.toByteArray();
+        HelloWorldRequest parsedRequest = HelloWorldRequest.parseFrom(requestBytes);
+
+        assertThat(parsedRequest.getRequestId()).isEqualTo(request.getRequestId());
+        assertThat(parsedRequest.getMessage()).isEqualTo(request.getMessage());
+    }
+
+    @Test
+    public void buildRequestFrom_writeBytesToFile_deserializeSuccessfully() throws IOException {
+        //when
+        HelloWorldRequest request = HelloWorldRequest.newBuilder()
+                .setRequestId("My request id")
+                .setMessage("Some message")
+                .build();
+        try(FileOutputStream fos = new FileOutputStream("hello-world")) {
+            request.writeTo(fos);
+        }
+
+        //then
+        try( FileInputStream fis = new FileInputStream("hello-world")) {
+            HelloWorldRequest deserializedRequest = HelloWorldRequest.newBuilder().mergeFrom(fis).build();
+
+            assertThat(deserializedRequest.getRequestId()).isEqualTo(request.getRequestId());
+            assertThat(deserializedRequest.getMessage()).isEqualTo(request.getMessage());
+        }
+    }
+}
